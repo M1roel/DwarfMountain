5593ffe877645b039b125be12199cd90f37c690e
M1roel <ppfautsch@yahoo.de>
2025-04-14 16:29:23 +0000

    fix: remove redundant dwarf generation on grass in the game initialization

--- src/js/game.js
+++ src/js/game.js
@@ -10,10 +10,6 @@ const level = new Level(157, 73);
 canvas.width = level.width * TILE_SIZE;
 canvas.height = level.height * TILE_SIZE;
 const dwarves = [
-    generateDwarfOnGrass(level.tilemap),
-    generateDwarfOnGrass(level.tilemap),
-    generateDwarfOnGrass(level.tilemap),
-    generateDwarfOnGrass(level.tilemap),
     generateDwarfOnGrass(level.tilemap)
 ];
 
